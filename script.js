032c008d5790cec283ccb921ee83b84efcff8d7d
Karel Šťastný <karell29@icloud.com>
2023-06-16 13:06:22 +0200

    Přidány základní soubory, obrázky a data

---
+++ script.js
@@ -0,0 +1,55 @@
+const products = [{
+    id: 1,
+    name:"Xiaomi Redmi Note 10 Pro",
+    price: 10000,
+    image: "./images/1.JPG",
+    categories: "phone",
+},{
+    id: 2,
+    name:"Sony Xperia 1 IV 5G",
+    price: 28990,
+    image: "./images/2.JPG",
+    categories: "phone",
+},{
+    id: 3,
+    name:"Apple iPhone 14 Pro",
+    price: 29990,
+    image: "./images/3.JPG",
+    categories: "phone",
+},{
+    id: 4,
+    name:"Dell G15 Gaming",
+    price: 39990,
+    image: "./images/4.JPG",
+    categories: "notebook",
+},{
+    id: 5,
+    name:"Lenovo Legion Pro 7",
+    price: 79989,
+    image: "./images/5.JPG",
+    categories: "notebook",
+},{
+    id: 6,
+    name:"GIGABYTE AORUS 15 XE4",
+    price: 47992,
+    image: "./images/6.JPG",
+    categories: "notebook",
+},{
+    id: 7,
+    name:"Apple iPad Pro Wi-Fi, 12.9",
+    price: 38490,
+    image: "./images/7.JPG",
+    categories: "tablet",
+},{
+    id: 8,
+    name:"Lenovo TAB P12 PRO",
+    price: 18563,
+    image: "./images/8.JPG",
+    categories: "tablet",
+},{
+    id: 9,
+    name:"Xiaomi Pad 5",
+    price: 8190,
+    image: "./images/9.JPG",
+    categories: "tablet",
+},]
